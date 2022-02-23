51831b1d3126e543026452fe674eed870d72a9f6
sunfkkc <rlcks77@nate.com>
2022-02-24 01:40:25 +0900

    MERN

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,36 +1,38 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = asyncHandler(async(req,res)=>{
-    console.log(req.body)
-    res.status(200).json({'mgs':'get goals'})
+const getGoals = asyncHandler(async (req, res) => {
+    const goals = await Goal.find() //Goal = mongoose.model('Goal', goalSchema)
+    res.status(200).json({ goals })
 })
 
 // @desc    Set goals
 // @route   POST /api/goals
 // @access  Private
-const postGoals = asyncHandler(async(req,res)=>{
-    if(!req.body.text){
+const postGoals = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
-    else{
-        res.status(200).json({'mgs':'put goals'})
-    }
+    const goal = await Goal.create({
+        text: req.body.text,
+    })
+    res.status(200).json(goal)
 })
 
 // @desc    Update goals
 // @route   PUT /api/goals/:id
 // @access  Private
-const putGoals = asyncHandler(async(req,res)=>{
-    res.status(200).json({'mgs':`put goals ${req.params.id}`})
+const putGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({ 'mgs': `put goals ${req.params.id}` })
 })
 
 // @desc    Delete goals
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoals = asyncHandler(async(req,res)=>{
-    res.status(200).json({'mgs':`delete goals ${req.params.id}`})
+const deleteGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({ 'mgs': `delete goals ${req.params.id}` })
 })
-module.exports = {getGoals,postGoals,putGoals,deleteGoals}
+module.exports = { getGoals, postGoals, putGoals, deleteGoals }
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const port = process.env.port || 5000
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db.js')
 
-
-
+connectDB()
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use('/api/goals',require('./routes/goalRoutes'))
+app.use(express.urlencoded({ extended: false }))
+app.use('/api/goals', require('./routes/goalRoutes'))
 app.use(errorHandler)
 
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
